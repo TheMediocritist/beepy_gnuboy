b026bd761d2f227d6fc85c5ddeb005719e802d6c
TheMediocritist <79881777+TheMediocritist@users.noreply.github.com>
2023-07-18 15:12:17 +1000

    Update fbdev.c

--- sys/linux/fbdev.c
+++ sys/linux/fbdev.c
@@ -221,12 +221,12 @@ static void framebuffer_copy()
 			if ((y % 5) % 2 == 0)
 			{
 				// For even rows, copy a single row from the source area
-				src_pos_y = src_y + (int) (src_height * (y/dest_height));
+				src_pos_y = src_y + (int) (src_height * (double) (y/dest_height));
 			}
 			else
 			{
 				// For odd rows, duplicate a single row from the source area
-				src_pos_y = src_y + (int) (src_height * (y/(dest_height-1)));
+				src_pos_y = src_y + (int) (src_height * (double) (y/(dest_height-1)));
 			}
 	
 			for (x = 0; x < dest_width; x++)
