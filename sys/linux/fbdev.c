30fc9d32fd0e7b449065261cc71d325ee94314ed
TheMediocritist <79881777+TheMediocritist@users.noreply.github.com>
2023-07-18 10:40:52 +1000

    Update fbdev.c

--- sys/linux/fbdev.c
+++ sys/linux/fbdev.c
@@ -221,8 +221,8 @@ static void framebuffer_copy()
 			{
 				int src_x = 480 + b;
 				int dst_x = 160 + b; 
-				fbmap[(row-60)*1600 + dst_x] = new_fbmap[row*1600+src_x];
-				fbmap[(row-60)*1600 + dst_x + 1600] = new_fbmap[row*1600+src_x];
+				fbmap[(row-60)*1600 + dst_x*2] = new_fbmap[row*1600+src_x];
+				//fbmap[(row-60)*1600 + dst_x + 1600] = new_fbmap[row*1600+src_x];
 				//fbmap[(row-60)*3200+b+1600] = new_fbmap[row*1600+b];
 			}
 		}
