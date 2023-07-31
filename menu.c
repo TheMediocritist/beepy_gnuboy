bbf5ded09ba4ee3d7b6ee0e619fac9088759ca67
TheMediocritist <79881777+TheMediocritist@users.noreply.github.com>
2023-07-31 23:04:56 +1000

    Update menu.c

--- menu.c
+++ menu.c
@@ -197,8 +197,8 @@ static void menu_paint(void) {
 	// scan.pal4[1] = 0xffffffff; // alpha left or right ?
 	// scan.pal4[2] = 0x66666666;
 	
-	scan.pal1[0] = 0xffffffff;
-	scan.pal1[1] = 0;
+	scan.pal1[0] = 0;
+	scan.pal1[1] = 0xffffffff;
 	scan.pal1[2] = 0;
 	scan.pal2[0] = 0;
 	scan.pal2[1] = 0;
