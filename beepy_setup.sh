889d8a7ac45eb14b7c9e324431cc1cdbd67fd5dc
TheMediocritist <79881777+TheMediocritist@users.noreply.github.com>
2023-08-02 23:05:53 +1000

    Update beepy_setup.sh

--- beepy_setup.sh
+++ beepy_setup.sh
@@ -4,9 +4,11 @@ set -e
 echo "Downloading from github..."
 git clone https://github.com/TheMediocritist/beepy_gnuboy || { echo "Error: Failed to clone display driver repository."; exit 1; }
 cd beepy_gnuboy
+./configure
 make || { echo "Error: Failed to compile."; exit 1; }
 
 echo "Installing binary and config file"
+make install
 cp fbgnuboy /usr/local/bin
 mkdir ~/.gnuboy
 cp gnuboy.rc ~/.gnuboy/gnuboy.rc
