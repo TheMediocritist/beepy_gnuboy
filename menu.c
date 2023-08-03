fa20a16dbaa8bf544542ac8eb2a4fea33aca887a
TheMediocritist <79881777+TheMediocritist@users.noreply.github.com>
2023-08-03 16:26:38 +1000

    Update menu.c

--- menu.c
+++ menu.c
@@ -208,7 +208,7 @@ static void menu_paint(void) {
 		for (x = 0; x < ezm.w; ++x) {
 			if (y == ezm.vissel) {
 				if (x == 0) {
-					font_blit(screen, x * FONTW, y * FONTH, '>', 0); // Draw the '>' character
+					font_blit(screen, (x + 1) * FONTW, y * FONTH, '>', 0); // Draw the '>' character
 				}
 				else if (x == 1) {
 					font_blit(screen, (x + 1) * FONTW, y * FONTH, '_', 0); // Draw the '_' character
@@ -217,7 +217,7 @@ static void menu_paint(void) {
 					font_blit(screen, (x + 2) * FONTW, y * FONTH, x >= l ? ' ' : ezm.vislines[y][x], 0);
 				}
 			} else {
-				font_blit(screen, (x + 3) * FONTW, y * FONTH, x >= l ? ' ' : ezm.vislines[y][x], 0);
+				font_blit(screen, (x + 2) * FONTW, y * FONTH, x >= l ? ' ' : ezm.vislines[y][x], 0);
 			}
 		}
 	}
