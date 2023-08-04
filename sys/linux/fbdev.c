e3d93c2654eafc7a823836449d429b9d12d87ace
TheMediocritist <79881777+TheMediocritist@users.noreply.github.com>
2023-08-04 11:47:15 +1000

    Update fbdev.c

--- sys/linux/fbdev.c
+++ sys/linux/fbdev.c
@@ -231,8 +231,8 @@ static void framebuffer_copy()
 	
 				// Copy one pixel at a time (4 bytes)
 				memcpy(&fbmap[(dest_pos_y * vi.xres_virtual + dest_pos_x) * 4],
-					   &new_fbmap[(src_pos_y * vi.xres_virtual + src_pos_x) * 4],
-					   4);
+					   &new_fbmap[(src_pos_y * vi.xres_virtual + src_pos_x)],
+					   1);
 			}
 		}
 	}
